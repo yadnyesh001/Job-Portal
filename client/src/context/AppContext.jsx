0cf99341b040765adcce5393417374e3fe15f814
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-03-15 00:25:42 +0530

    hero component

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,19 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  const value = {
 
+  const [searchFilter, setSearchFilter] = useState({
+    title: '',
+    location: ''
+  });
+
+  const [isSearched, setIsSearched] = useState(false);
+
+  const value = {
+    setSearchFilter, searchFilter,
+    setIsSearched, isSearched
   }
   return (
     <AppContext.Provider value={value}>
